6d362016eec707f40e7bd595a982dc3d736983e4
ENGİN GÖKMEN <engingokmen@ENGINs-MacBook-Pro.local>
2024-09-30 20:44:22 +0300

    adds scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.scss'
 
 const root = createRoot(document.getElementById('app'))
 root.render(<h1>Hello, world</h1>)
