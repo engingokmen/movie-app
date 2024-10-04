4524c7d2ee12447d054ed387c2205af751db52f2
ENGİN GÖKMEN <engingokmen@192.168.1.41>
2024-10-04 12:33:36 +0300

    adds addtoFavorites button to detail page

--- src/features/detail/index.tsx
+++ src/features/detail/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { getMovieById, useMovies } from '../../context/movies'
 import { ImdbScore } from '../../components/ImdbScore'
+import { AddToFavoritesButton } from '../favorites/AddToFavoritesButton'
 
 export const Detail = () => {
     const { id } = useParams()
@@ -9,22 +10,25 @@ export const Detail = () => {
     const movie = getMovieById(movies, id)
 
     return (
-        <div className="movie-detail">
+        <>
             <Link to="/movies" className="back-link">
                 Back to movies
             </Link>
-            <div className="poster-container">
-                <img src={movie.poster} alt={movie.name} />
+            <div className="movie-detail">
+                <AddToFavoritesButton id={movie.id} />
+                <div className="poster-container">
+                    <img src={movie.poster} alt={movie.name} />
+                </div>
+                <p className="movie-summary">{movie.summary}</p>
+                <div className="seperator"></div>
+                <div className="movie-card">
+                    <ImdbScore score={movie.imdb} />
+                    <p className="text-gray-small">{movie.category}</p>
+                    <p className="text-gray-small">
+                        {movie.country}, {movie.year}
+                    </p>
+                </div>
             </div>
-            <p className="movie-summary">{movie.summary}</p>
-            <div className="seperator"></div>
-            <div className="movie-card">
-                <ImdbScore score={movie.imdb} />
-                <p className="text-gray-small">{movie.category}</p>
-                <p className="text-gray-small">
-                    {movie.country}, {movie.year}
-                </p>
-            </div>
-        </div>
+        </>
     )
 }
