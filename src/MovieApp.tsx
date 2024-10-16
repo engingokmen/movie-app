3b22709db8ec9f0fd4c7977fe49de911dc064482
Engin Gokmen <engin.gokmen@icloud.com>
2024-10-16 15:38:42 +0300

    updates index file

--- src/MovieApp.tsx
+++ src/MovieApp.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import { router } from './routes'
 import { RouterProvider } from 'react-router-dom'
+import { AuthProvider } from './context/authenticate'
+import { MoviesProvider } from './context/movies'
+import { FavoritesProvider } from './context/favorites'
+import { FilterProvider } from './context/filters'
 
 export const MovieApp = () => {
-    return <RouterProvider router={router} />
+    return (
+        <React.StrictMode>
+            <AuthProvider>
+                <MoviesProvider>
+                    <FilterProvider>
+                        <FavoritesProvider>
+                            <RouterProvider router={router} />
+                        </FavoritesProvider>
+                    </FilterProvider>
+                </MoviesProvider>
+            </AuthProvider>
+        </React.StrictMode>
+    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.scss'
 import { MovieApp } from './MovieApp'
-import { AuthProvider } from './context/authenticate'
-import { MoviesProvider } from './context/movies'
-import { FavoritesProvider } from './context/favorites'
-import { FilterProvider } from './context/filters'
 
 const root = createRoot(document.getElementById('app') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <AuthProvider>
-            <MoviesProvider>
-                <FilterProvider>
-                    <FavoritesProvider>
-                        <MovieApp />
-                    </FavoritesProvider>
-                </FilterProvider>
-            </MoviesProvider>
-        </AuthProvider>
-    </React.StrictMode>
-)
+root.render(<MovieApp />)
+
+export { MovieApp }
